efdec68671ecb3e295df1de3a22f5f28b94e89fe
luzialeticia <leticia.luzia@thoughtworks.com>
2022-02-15 09:44:11 -0300

    Create Team Model and TeamsViewController

---
+++ Futebol Feminino BR/Model/Team.swift
@@ -0,0 +1,17 @@
+//
+//  Team.swift
+//  Futebol Feminino BR
+//
+//  Created by laleticia.luzia on 15/02/22.
+//
+
+import Foundation
+
+struct Team {
+    let name: String
+    let initials: String
+    let division: String
+    let state: String
+    let logo: String
+    
+}
---
+++ Futebol Feminino BR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Futebol Feminino BR
+//
+//  Created by laleticia.luzia on 15/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
